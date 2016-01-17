f90665dc42087ef59127d6e408d6cafe5250b850
Vasil Todorovski <vasko16@gmail.com>
2016-01-18 00:01:33 +0100

    Initial commit

---
+++ WebContent/sapui5-exploration/mainView.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("sapui5-exploration.mainView", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf sapui5-exploration.mainView
+	*/ 
+	getControllerName : function() {
+		return "sapui5-exploration.mainView";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf sapui5-exploration.mainView
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
