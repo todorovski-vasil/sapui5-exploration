c29bdfcfe37e386024f9e6533917ae1f537fde96
Vasil Todorovski <vasko16@gmail.com>
2016-02-02 12:52:30 +0100

    Completed: - Filtering - Sorting and Grouping
    Tried a few variations and advanced options with the sorting and filtering

--- WebContent/controller/invoiceList.controller.js
+++ WebContent/controller/invoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"sapui5-exploration/model/formatter"
-], function (Controller, JSONModel, formatter) {
+	"sapui5-exploration/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("controller.invoiceList", {
@@ -13,6 +15,25 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+		onFilterInvoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var oFilter = null;
+//			var sQuery = oEvent.getParameter("query");
+			var sQuery = oEvent.getParameter("newValue");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+				aFilter.push(new Filter("Quantity", FilterOperator.EQ, sQuery));
+				
+				oFilter = new Filter(aFilter, false);
+			}
+			
+			// filter binding
+			var oList = this.getView().byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(oFilter);			
 		}
 	});
 });
